0331e41d89be26c1dc2463f55bf921e2d6ee6daa
lihaoming <lihaoming@sunboxsoft.com>
2020-06-18 11:20:57 +0800

    feat:初始上传

---
+++ dubbo-api/src/main/java/com/omen/service/PayService.java
@@ -0,0 +1,11 @@
+package com.omen.service;
+
+/**
+ * @Description:
+ * @Author: lihaoming
+ * @Date: 2020/6/17 18:15
+ */
+public interface PayService {
+
+     String pay();
+}
---
+++ dubbo-api/src/main/java/com/omen/service/UserService.java
@@ -0,0 +1,11 @@
+package com.omen.service;
+
+/**
+ * @Description:
+ * @Author: lihaoming
+ * @Date: 2020/6/18 8:46
+ */
+public interface UserService {
+
+    String getName();
+}
