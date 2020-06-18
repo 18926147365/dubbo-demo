ab869f6405494032a9200be38cf7a42862ee050b
lihaoming <lihaoming@sunboxsoft.com>
2020-06-18 17:25:26 +0800

    fix:修复Reference注解，使用apache版本的注释

--- dubbo-consumer/src/main/java/com/omen/controller/UserController.java
+++ dubbo-consumer/src/main/java/com/omen/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.omen.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.omen.service.PayService;
 import com.omen.service.UserService;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
